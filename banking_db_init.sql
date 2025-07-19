de38440e3eccc4b51c07bf6c465009ce5f759ab6
davidelatina <davide.latina616@gmail.com>
2025-07-19 12:40:39 +0200

    Create banking_db_init.sql
    Initial draft of bank database structure

---
+++ banking_db_init.sql
@@ -0,0 +1,63 @@
+
+-- create demo scheme
+CREATE DATABASE banking_db;
+
+USE banking_db;
+
+-- customers
+CREATE TABLE customer (
+    -- SERIAL is an alias for BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    age INT UNSIGNED NOT NULL,
+    registered_at DATETIME NOT NULL
+);
+
+-- account types: checking, savings
+CREATE TABLE account_type (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+INSERT INTO account_type (name) VALUES 
+    ("checking"),
+    ("savings");
+
+-- accounts
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    customer_id BIGINT UNSIGNED NOT NULL, -- FK to customer
+    account_type_id INT UNSIGNED NOT NULL, -- FK to account_type
+    balance DECIMAL(18,9) UNSIGNED NOT NULL,
+    created_at DATETIME NOT NULL,
+    
+    FOREIGN KEY (customer_id) REFERENCES customer(id),
+    FOREIGN KEY (account_type_id) REFERENCES account_type(id)
+);
+
+-- transaction types: deposit, withdrawal, transfer
+CREATE TABLE transaction_type (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+INSERT INTO transaction_type (name) VALUES 
+    ("deposit"),
+    ("withdrawal"),
+    ("transfer");
+
+-- transactions
+CREATE TABLE transaction (
+    id SERIAL PRIMARY KEY,
+    account_id BIGINT UNSIGNED NOT NULL, -- FK to account
+    type_id INT UNSIGNED NOT NULL, -- FK to transaction_type
+    amount DECIMAL(18,9) UNSIGNED NOT NULL,
+    -- to be added: recipient
+    executed_at DATETIME NOT NULL,
+    
+    FOREIGN KEY (account_id) REFERENCES account(id),
+    FOREIGN KEY (type_id) REFERENCES transaction_type(id)
+);
+
+
